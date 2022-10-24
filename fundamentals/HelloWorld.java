d039be06d0091014c5117fe25bc97e34a2bb3704
anani <abood.anani57@gmail.com>
2022-10-24 11:23:15 +0300

    initial

---
+++ fundamentals/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    String name = "Abdulrahman";
+    int age = 30;
+    String homeTown = "Hebron";
+    System.out.println(String.format("My name is %s, I am  %x, My home town is %s",  name, age, homeTown));
+  }
+}
